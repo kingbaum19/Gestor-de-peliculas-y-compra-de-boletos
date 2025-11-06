11c374cf07338519ee9b8244ef75217db5e769e7
kingbaum19 <briantguapo1@gmail.com>
2025-11-06 03:39:10 -0500

    Renovación visual en las vistas utilizando los controladores personalizados e imágenes en los paneles y botones.

--- Prototipo/Vistas/LoginForm.Designer.cs
+++ Prototipo/Vistas/LoginForm.Designer.cs
@@ -30,14 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             label1 = new Label();
-            panel1 = new Panel();
             label2 = new Label();
-            txtEmail = new TextBox();
-            txtContraseña = new TextBox();
-            btnLogin = new Button();
             lblRegister = new Label();
             lblRecuperarContraseña = new Label();
             btnSalir = new Button();
+            txtCorreo = new Prototipo.Custom_Controls.CustomTextbox();
+            txtContraseña = new Prototipo.Custom_Controls.CustomTextbox();
+            btnIniciarSesion = new Prototipo.Custom_Controls.CustomButton();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -50,15 +51,6 @@
             label1.TabIndex = 1;
             label1.Click += label1_Click;
             // 
-            // panel1
-            // 
-            panel1.BackColor = Color.BlueViolet;
-            panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(250, 561);
-            panel1.TabIndex = 2;
-            // 
             // label2
             // 
             label2.AutoSize = true;
@@ -69,40 +61,11 @@
             label2.TabIndex = 3;
             label2.Text = "Bienvenido al portal de administrador";
             // 
-            // txtEmail
-            // 
-            txtEmail.ForeColor = Color.Gray;
-            txtEmail.Location = new Point(348, 208);
-            txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(427, 23);
-            txtEmail.TabIndex = 4;
-            txtEmail.Text = "Correo electrónico";
-            // 
-            // txtContraseña
-            // 
-            txtContraseña.ForeColor = Color.DarkGray;
-            txtContraseña.Location = new Point(348, 272);
-            txtContraseña.Name = "txtContraseña";
-            txtContraseña.PasswordChar = '*';
-            txtContraseña.Size = new Size(427, 23);
-            txtContraseña.TabIndex = 5;
-            txtContraseña.Text = "Contraseña";
-            // 
-            // btnLogin
-            // 
-            btnLogin.Location = new Point(502, 365);
-            btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(116, 23);
-            btnLogin.TabIndex = 6;
-            btnLogin.Text = "Iniciar Sesión";
-            btnLogin.UseVisualStyleBackColor = true;
-            btnLogin.Click += button1_Click;
-            // 
             // lblRegister
             // 
             lblRegister.AutoSize = true;
             lblRegister.ForeColor = Color.RoyalBlue;
-            lblRegister.Location = new Point(481, 337);
+            lblRegister.Location = new Point(499, 334);
             lblRegister.Name = "lblRegister";
             lblRegister.Size = new Size(163, 15);
             lblRegister.TabIndex = 7;
@@ -112,7 +75,7 @@
             // 
             lblRecuperarContraseña.AutoSize = true;
             lblRecuperarContraseña.ForeColor = Color.RoyalBlue;
-            lblRecuperarContraseña.Location = new Point(478, 406);
+            lblRecuperarContraseña.Location = new Point(496, 412);
             lblRecuperarContraseña.Name = "lblRecuperarContraseña";
             lblRecuperarContraseña.Size = new Size(166, 15);
             lblRecuperarContraseña.TabIndex = 8;
@@ -135,36 +98,107 @@
             btnSalir.UseVisualStyleBackColor = false;
             btnSalir.Click += button2_Click;
             // 
-            // Form1
+            // txtCorreo
+            // 
+            txtCorreo.BackColor = SystemColors.Window;
+            txtCorreo.BorderColor = Color.DarkViolet;
+            txtCorreo.BorderFocusColor = SystemColors.MenuHighlight;
+            txtCorreo.BorderRadius = 0;
+            txtCorreo.BorderSize = 2;
+            txtCorreo.Font = new Font("Segoe UI", 10F);
+            txtCorreo.ForeColor = SystemColors.WindowText;
+            txtCorreo.Location = new Point(345, 182);
+            txtCorreo.MaxLength = 32767;
+            txtCorreo.Multiline = false;
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Padding = new Padding(7);
+            txtCorreo.PasswordChar = false;
+            txtCorreo.PlaceholderColor = Color.DarkGray;
+            txtCorreo.PlaceholderText = "Correo electrónico";
+            txtCorreo.Size = new Size(457, 34);
+            txtCorreo.TabIndex = 10;
+            txtCorreo.Texto = "";
+            txtCorreo.UnderlinedStyle = true;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.BackColor = SystemColors.Window;
+            txtContraseña.BorderColor = Color.DarkOrchid;
+            txtContraseña.BorderFocusColor = SystemColors.MenuHighlight;
+            txtContraseña.BorderRadius = 0;
+            txtContraseña.BorderSize = 2;
+            txtContraseña.Font = new Font("Segoe UI", 10F);
+            txtContraseña.ForeColor = SystemColors.WindowText;
+            txtContraseña.Location = new Point(345, 261);
+            txtContraseña.MaxLength = 32767;
+            txtContraseña.Multiline = false;
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Padding = new Padding(7);
+            txtContraseña.PasswordChar = true;
+            txtContraseña.PlaceholderColor = Color.DarkGray;
+            txtContraseña.PlaceholderText = "Contraseña";
+            txtContraseña.Size = new Size(457, 34);
+            txtContraseña.TabIndex = 12;
+            txtContraseña.Texto = "";
+            txtContraseña.UnderlinedStyle = true;
+            // 
+            // btnIniciarSesion
+            // 
+            btnIniciarSesion.BackColor = Color.BlueViolet;
+            btnIniciarSesion.FlatAppearance.BorderSize = 0;
+            btnIniciarSesion.FlatStyle = FlatStyle.Flat;
+            btnIniciarSesion.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnIniciarSesion.ForeColor = Color.White;
+            btnIniciarSesion.Location = new Point(499, 361);
+            btnIniciarSesion.Name = "btnIniciarSesion";
+            btnIniciarSesion.Size = new Size(163, 40);
+            btnIniciarSesion.TabIndex = 13;
+            btnIniciarSesion.Text = "Iniciar sesión";
+            btnIniciarSesion.UseVisualStyleBackColor = false;
+            btnIniciarSesion.Click += customButton1_Click_1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(293, 561);
+            pictureBox1.TabIndex = 14;
+            pictureBox1.TabStop = false;
+            // 
+            // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(854, 561);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnIniciarSesion);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtCorreo);
             Controls.Add(btnSalir);
             Controls.Add(lblRecuperarContraseña);
             Controls.Add(lblRegister);
-            Controls.Add(btnLogin);
-            Controls.Add(txtContraseña);
-            Controls.Add(txtEmail);
             Controls.Add(label2);
-            Controls.Add(panel1);
             Controls.Add(label1);
-            Name = "Form1";
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += LoginForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
         private Label label1;
-        private Panel panel1;
         private Label label2;
-        private TextBox txtEmail;
-        private TextBox txtContraseña;
-        private Button btnLogin;
         private Label lblRegister;
         private Label lblRecuperarContraseña;
         private Button btnSalir;
+        private Custom_Controls.CustomTextbox txtCorreo;
+        private Custom_Controls.CustomTextbox txtContraseña;
+        private Custom_Controls.CustomButton btnIniciarSesion;
+        private PictureBox pictureBox1;
     }
 }
