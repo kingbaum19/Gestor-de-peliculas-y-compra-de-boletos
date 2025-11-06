8b635a1050144b962acc197a8c4712eabb323fe8
kingbaum19 <briantguapo1@gmail.com>
2025-11-06 03:37:51 -0500

    Añadido controladores personalizados de TextBox y botones, estos permiten que el textbox tenga diferentes estilos y que ambos controladores tengan bordes con radio ajustable.

---
+++ Prototipo/Custom Controls/CustomTextbox.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Prototipo.Custom_Controls
+{
+    partial class CustomTextbox
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(7, 7);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 18);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.Enter += textBox1_Enter;
+            textBox1.Leave += textBox1_Leave;
+            // 
+            // CustomTextbox
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = SystemColors.Window;
+            Controls.Add(textBox1);
+            Font = new Font("Segoe UI", 10F);
+            ForeColor = SystemColors.GrayText;
+            Name = "CustomTextbox";
+            Padding = new Padding(7);
+            Size = new Size(250, 30);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
