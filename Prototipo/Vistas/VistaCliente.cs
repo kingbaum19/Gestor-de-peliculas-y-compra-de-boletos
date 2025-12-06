f8d8b1aa1603a18fedd43547a3603324334d1653
kingbaum19 <briantguapo1@gmail.com>
2025-12-06 17:28:52 -0500

    Vista del cliente

---
+++ Prototipo/Vistas/VistaCliente.cs
@@ -0,0 +1,36 @@
+﻿using Prototipo.Controladores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo.Vistas
+{
+    public partial class VistaCliente : Form
+    {
+        public VistaCliente()
+        {
+            InitializeComponent();
+            PeliculaController objetoPelicula = new PeliculaController();
+            objetoPelicula.mostrarPelicula(pbCaratula1, 22);
+            objetoPelicula.mostrarPelicula(pbCaratula2, 27);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            DetallesPelicula detalles = new DetallesPelicula();
+            detalles.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
